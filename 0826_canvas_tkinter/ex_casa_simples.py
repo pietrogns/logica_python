16cce71fb910c85b8e002f613b357c42df90e926
pietrogns <pietronascimento010@gmail.com>
2026-08-28 22:16:05 -0300

    08/28

--- 0826_canvas_tkinter/ex_casa_simples.py
+++ 0826_canvas_tkinter/ex_casa_simples.py
@@ -1,40 +1,12 @@
 from tkinter import Tk, Canvas
-
 janela = Tk()
 janela.geometry("800x600")
 
 canvas = Canvas(janela, width=700, height=500, bg="aqua")
-
-
-
-# canvas.create_line(
-#    110, 110, 150, 110,
-#    fill="blue",
-#    width=3)
-
-# #linha reta para baixo
-# canvas.create_line(
-#    200, 200, 110, 200,
-#    fill="orange",
-#    width=3)
-
-# #linha reta para baixo
-# canvas.create_line(
-#    200, 200, 200, 110,
-#    fill="purple",
-#    width=3)
-
 #Base da casa
-canvas.create_rectangle(
-    300,250, 150, 150,
-    fill="white"
-)
-
+canvas.create_rectangle(300,250, 150, 150,fill="white")
 #Triângulo
-canvas.create_polygon(220,30,
-                      300,150
-                      ,150,150,
-                      fill="brown")
+canvas.create_polygon(220,40,300,150,150,150,fill="brown")
 
 #Janela esquerda
 
@@ -43,31 +15,26 @@ canvas.create_line(
    190, 210, 190, 180,
    fill="brown",
    width=3)
-
 #linha para baixo esq
 canvas.create_line(
    160, 210, 160, 180,
    fill="brown",
    width=3)
-
 #linha reta baixo
 canvas.create_line(
    159, 210, 192, 210,
    fill="brown",
    width=3)
-
 #linha reta de cima
 canvas.create_line(
    159, 180, 192, 180,
    fill="brown",
    width=3)
-
 #linha do meio para baixo
 canvas.create_line(
    175, 210, 175, 180,
    fill="gray",
    width=3)
-
 #linha reta do meio
 canvas.create_line(
    159, 195, 190, 195,
@@ -77,58 +44,41 @@ canvas.create_line(
 #Janela direita
 
 #linha para baixo dir
-canvas.create_line(
-   290, 210, 290, 180,
-   fill="brown",
-   width=3)
+canvas.create_line(290, 210, 290, 180,fill="brown",width=3)
 #linha para baixo esq
-canvas.create_line(
-   260, 210, 260, 180,
-   fill="brown",
-   width=3)
-
+canvas.create_line(260, 210, 260, 180,fill="brown",width=3)
 #linha reta de baixo
-canvas.create_line(
-   259, 210, 292, 210,
-   fill="brown",
-   width=3)
-
+canvas.create_line(259, 210, 292, 210,fill="brown",width=3)
 #linha reta de cima
-canvas.create_line(
-   259, 180, 292, 180,
-   fill="brown",
-   width=3)
-
+canvas.create_line(259, 180, 292, 180,fill="brown",width=3)
 #linha do meio para baixo
-canvas.create_line(
-   275, 210, 275, 180,
-   fill="gray",
-   width=3)
-
+canvas.create_line(275, 210, 275, 180,fill="gray",width=3)
 #linha reta do meio
-canvas.create_line(
-   259, 195, 292, 195,
-   fill="gray",
-   width=3)
-
+canvas.create_line(259, 195, 292, 195,fill="gray",width=3)
 
 #Porta
-canvas.create_rectangle(
-    200, 250, 250, 190,
-    fill="light gray"
-)
-
+canvas.create_rectangle(200, 250, 250, 190,fill="light gray")
 #Maçaneta
-canvas.create_oval(
-    202,220, 210, 215,
-    fill="black"
-)
-
+canvas.create_oval(202,220, 210, 215,fill="black")
 #Grama
-canvas.create_rectangle(
-    800,600, 1, 250,
-    fill="green"
-)
+canvas.create_rectangle(800,600, 1, 250,fill="green")
+
+#CARRO
+
+#linha para baixo dir
+canvas.create_rectangle(602, 215, 560, 170,fill="brown",width=3)
+#linha para baixo esq
+canvas.create_rectangle(560, 215, 560, 170,fill="brown",width=3)
+#linha reta de baixo
+canvas.create_line(559, 215, 602, 215,fill="brown",width=3)
+#linha reta de cima
+canvas.create_line(559, 170, 602, 170,fill="brown",width=3)
+#linha do meio para baixo
+canvas.create_line(275, 210, 275, 180,fill="gray",width=3)
+#linha reta do meio
+canvas.create_line(259, 195, 292, 195,fill="gray",width=3)
+
+
 
 canvas.pack()
 janela.mainloop()
